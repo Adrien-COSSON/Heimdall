1fdcd83cab56e25e4346b677a888f9780fe89e6b
adriencosson-beep <adrien.cosson@gmail.com>
2026-07-12 00:11:11 +0200

    Adding config.py file containing the configuration values for Heimdall monitoring

---
+++ config.py
@@ -0,0 +1,19 @@
+# Import libraries
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Loading BOT_TOKEN and CHAT_ID for Telegram
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+
+# Critical variables
+# Temp (for temperature in Celsius degrees)
+# Usage in percentage
+# Polling intervalle in seconds
+critical_temp_CPU = 90
+critical_temp_harddrive = 60
+max_ram_usage = 95
+max_cpu_usage = 95
+max_harddrive_usage = 90
+polling_intervalle = 60
