2c47df89fd44fcbf083e4921bd08f0144ba37d67
adriencosson-beep <adrien.cosson@gmail.com>
2026-07-13 04:46:53 +0200

    feat: adding try/except in telegram.py

--- alerting/telegram.py
+++ alerting/telegram.py
@@ -1,12 +1,18 @@
-# Import library
 import requests
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Loading BOT_TOKEN and CHAT_ID for Telegram
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 
 def send_alert(message):
-    return requests.post(url=f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", json={"chat_id": CHAT_ID, "text": message})
+    try:
+        response = requests.post(
+            url=f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+            json={"chat_id": CHAT_ID, "text": message}
+        )
+        response.raise_for_status()
+        return response
+    except requests.exceptions.RequestException as e:
+        print(f"Telegram error: {e}")
